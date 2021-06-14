4e3ad7fac18f8975853be56620aeef04391c60c7
Jan Rueckert <jan.nr@live.de>
2021-06-14 19:03:01 +0200

    passing over place to addFavourites component

--- src/Button/AddFavourites.tsx
+++ src/Button/AddFavourites.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import Bookmark from "@material-ui/icons/Bookmark";
 import IconButton from "@material-ui/core/IconButton";
+import { Place } from "../types";
 
-const AddFavourite: React.FC = () => {
+interface Props {
+  selectedPlace?: Place;
+}
+
+const AddFavourite: React.FC<Props> = (props) => {
+  const { selectedPlace } = props;
+  if (!selectedPlace) return <></>;
   return (
     <>
       <IconButton>
