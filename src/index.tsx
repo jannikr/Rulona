2508924df2db254b3eafb50d030ff70f0d4b2900
Robert Beilich <robert@beilich.de>
2021-07-20 17:37:25 +0200

    fix(theme): Fix typing and linting
    material ui theme typings do not accept string shorthand

--- src/index.tsx
+++ src/index.tsx
@@ -10,17 +10,19 @@ import reportWebVitals from "./reportWebVitals";
 
 const theme = createMuiTheme({
   palette: {
-    primary: '#0A524D',
-  }
+    primary: {
+      main: "#0A524D",
+    },
+  },
 });
 
 loadDynamicConstants().then(() => {
   ReactDOM.render(
     <React.StrictMode>
       <ThemeProvider theme={theme}>
-      <Provider store={store()}>
-        <App />
-      </Provider>
+        <Provider store={store()}>
+          <App />
+        </Provider>
       </ThemeProvider>
     </React.StrictMode>,
     document.getElementById("root")
