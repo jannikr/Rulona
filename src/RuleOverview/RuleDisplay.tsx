2a34da7fb2f44c9abaa01086010b3a1cd9b51eb2
timo.boersch <timo.boersch@gmx.de>
2021-07-26 16:57:21 +0200

    Implement imperfect search for rules

--- src/RuleOverview/RuleDisplay.tsx
+++ src/RuleOverview/RuleDisplay.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
 import { Rule } from "../types";
+import Highlighter from "./Higlighter";
+import ReactDOMServer from "react-dom/server";
 
 interface Props {
   rule: Rule;
-  searchWord?: string;
+  searchWord: string;
 }
 
 const RuleDisplay: React.FC<Props> = (props) => {
-  const { rule } = props;
-  return <ReactMarkdown children={rule.text} />;
+  const { rule, searchWord } = props;
+
+  const components = <Highlighter text={rule.text} searchWord={searchWord} />;
+  const test = ReactDOMServer.renderToStaticMarkup(components);
+
+  return <ReactMarkdown rehypePlugins={[rehypeRaw]} children={test} />;
 };
 
 export default RuleDisplay;
