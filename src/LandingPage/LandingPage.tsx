7d5b4debe244a73d0951187747ddc960414c617f
Jan Rueckert <jan.nr@live.de>
2021-07-28 13:55:26 +0200

    Add redux store to Landing page in order to hide components in different situation

--- src/LandingPage/LandingPage.tsx
+++ src/LandingPage/LandingPage.tsx
@@ -1,25 +1,59 @@
 import { Grid } from "@material-ui/core";
-import React from "react";
+import React, { useEffect } from "react";
 import RuleOverview from "../RuleOverview/RuleOverview";
 import Sidebar from "../Sidebar/Sidebar";
 import Hidden from "@material-ui/core/Hidden";
 import styles from "./LandingPage.module.css";
+import { connect } from "react-redux";
+import { AppState } from "../store/types";
+import { Place } from "../types";
 
-const LandingPage: React.FC = (): JSX.Element => {
+type Props = ReturnType<typeof mapStateToProps> & {
+  place: Place;
+};
+
+const LandingPage: React.FC<Props> = (props) => {
+  const { selectedPlace } = props;
+
+  useEffect(() => {
+    if (!selectedPlace) return;
+  }, [selectedPlace]);
+
+  if (!selectedPlace)
+    return (
+      <div className={styles.flex}>
+        <Grid container spacing={0}>
+          <Grid className={styles.sidebar} item xs={12} md={3}>
+            <Sidebar />
+          </Grid>
+          <Hidden smDown>
+            <Grid item xs={9}>
+              <RuleOverview />
+            </Grid>
+          </Hidden>
+        </Grid>
+      </div>
+    );
   return (
     <div className={styles.flex}>
       <Grid container spacing={0}>
-        <Grid className={styles.sidebar} item xs={12} md={3}>
-          <Sidebar />
-        </Grid>
         <Hidden smDown>
-          <Grid item xs={9}>
-            <RuleOverview />
+          <Grid className={styles.sidebar} item xs={12} md={3}>
+            <Sidebar />
           </Grid>
         </Hidden>
+        <Grid item xs={12} md={9}>
+          <RuleOverview />
+        </Grid>
       </Grid>
     </div>
   );
 };
 
-export default LandingPage;
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const mapStateToProps = (state: AppState) => {
+  const { selectedPlace } = state;
+  return { selectedPlace };
+};
+
+export default connect(mapStateToProps)(LandingPage);
