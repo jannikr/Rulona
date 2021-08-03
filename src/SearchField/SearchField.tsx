8927bf0a8ba57b83a316b532da1282ef2b21e906
timo.boersch <timo.boersch@gmx.de>
2021-08-03 18:59:00 +0200

    Implement back arrow

--- src/SearchField/SearchField.tsx
+++ src/SearchField/SearchField.tsx
@@ -2,7 +2,8 @@ import { InputAdornment, OutlinedInput } from "@material-ui/core";
 import React, { useCallback, useState, MouseEvent } from "react";
 import styles from "./SearchField.module.css";
 import SearchIcon from "@material-ui/icons/Search";
-import { Clear } from "@material-ui/icons";
+import { ArrowBackIos, Cancel } from "@material-ui/icons";
+import classnames from "classnames";
 
 interface Props {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -13,7 +14,10 @@ interface Props {
 const SearchField: React.FC<Props> = (props) => {
   const { onChange, onFocus, onBlur } = props;
 
-  const [icon, setIcon] = useState(<SearchIcon />);
+  const [icon, setIcon] = useState(
+    <SearchIcon className={styles.searchIconColor} />
+  );
+  const [backArrow, setBackArrow] = useState(<></>);
 
   const onMouseDown = (e: MouseEvent): void => {
     e.preventDefault();
@@ -25,17 +29,15 @@ const SearchField: React.FC<Props> = (props) => {
         setIcon(<></>);
       } else {
         setIcon(
-          <div
-            className={styles.searchButton}
+          <Cancel
+            className={classnames(styles.searchIconColor, styles.button)}
             onMouseDown={onMouseDown}
             onClick={(): void => {
               e.target.value = "";
               setIcon(<></>);
               onChange(e);
             }}
-          >
-            <Clear />
-          </div>
+          />
         );
       }
       onChange(e);
@@ -43,35 +45,62 @@ const SearchField: React.FC<Props> = (props) => {
     [onChange]
   );
 
-  const onFocusWrapper = useCallback(
-    (e: React.FocusEvent<HTMLInputElement>) => {
-      if (e.target.value === "") {
-        setIcon(<></>);
-      }
-      onFocus(e);
-    },
-    [onFocus]
-  );
-
   const onBlurWrapper = useCallback(
     (e: React.FocusEvent<HTMLInputElement>) => {
       if (e.target.value === "") {
-        setIcon(<SearchIcon />);
+        setIcon(<SearchIcon className={styles.searchIconColor} />);
+        setBackArrow(<></>);
+      } else {
+        setIcon(<></>);
       }
       onBlur(e);
     },
     [onBlur]
   );
 
+  const onFocusWrapper = useCallback(
+    (e: React.FocusEvent<HTMLInputElement>) => {
+      setBackArrow(
+        <ArrowBackIos
+          className={classnames(styles.button, styles.backArrow)}
+          onClick={(): void => {
+            e.target.value = "";
+            onBlurWrapper(e);
+          }}
+        />
+      );
+      if (e.target.value === "") {
+        setIcon(<></>);
+      } else {
+        setIcon(
+          <Cancel
+            className={classnames(styles.searchIconColor, styles.button)}
+            onMouseDown={onMouseDown}
+            onClick={(): void => {
+              e.target.value = "";
+              setIcon(<></>);
+              onChange(e);
+            }}
+          />
+        );
+      }
+      onFocus(e);
+    },
+    [onFocus, onChange, onBlurWrapper]
+  );
+
   return (
-    <OutlinedInput
-      className={styles.searchInput}
-      placeholder="Suche"
-      onChange={onChangeWrapper}
-      onFocus={onFocusWrapper}
-      onBlur={onBlurWrapper}
-      endAdornment={<InputAdornment position="end">{icon}</InputAdornment>}
-    />
+    <div className={styles.row}>
+      {backArrow}
+      <OutlinedInput
+        onChange={onChangeWrapper}
+        onFocus={onFocusWrapper}
+        onBlur={onBlurWrapper}
+        className={styles.searchInput}
+        placeholder="Suche"
+        endAdornment={<InputAdornment position="end">{icon}</InputAdornment>}
+      />
+    </div>
   );
 };
 
