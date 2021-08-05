925e65f2cd1a1486f323184538bdcd8c7a5595ef
timo.boersch <timo.boersch@gmx.de>
2021-08-05 15:33:44 +0200

    Add GoogleMap component to RoutePage

--- src/RoutePage/RoutePage.tsx
+++ src/RoutePage/RoutePage.tsx
@@ -2,14 +2,37 @@ import { Grid } from "@material-ui/core";
 import React from "react";
 import Sidebar from "../Sidebar/Sidebar";
 import styles from "../LandingPage/LandingPage.module.css";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { MAPS_API_KEY } from "../constants";
 
 const RoutePage: React.FC = () => {
+  const containerStyle = {
+    width: "100%",
+    height: "100%",
+  };
+
+  const center = {
+    lat: 51,
+    lng: 9,
+  };
+
   return (
     <Grid container spacing={0} className={styles.container}>
       <Grid className={styles.sidebar} item md={3}>
         <Sidebar />
       </Grid>
-      <Grid item md={9}></Grid>
+      <Grid item md={9}>
+        <LoadScript googleMapsApiKey={MAPS_API_KEY || ""}>
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={7}
+          >
+            {/* Child components, such as markers, info windows, etc. */}
+            <></>
+          </GoogleMap>
+        </LoadScript>
+      </Grid>
     </Grid>
   );
 };
--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
 import { DynamicConstants } from "./types";
 
+export const MAPS_API_KEY = process.env.REACT_APP_MAPS_API_KEY;
+
 export let dynamicConstants: DynamicConstants;
 
 export const loadDynamicConstants = async (): Promise<void> => {
