040fc8a41014cd3cdd5db1e89ac96d45bdf3d133
timo.boersch <timo.boersch@gmx.de>
2021-08-07 20:49:30 +0200

    Add redux store functionality for firstVisit

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -31,6 +31,11 @@ import {
   SetDestinationAction,
 } from "./types";
 
+export const setFirstVisit = (firstVisit: boolean): SetFirstVisitAction => ({
+  type: ActionType.SetFirstVisit,
+  firstVisit,
+});
+
 export const selectPlace = (place: Place): SelectPlaceAction => {
   return {
     type: ActionType.SelectPlace,
